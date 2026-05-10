96e423a0a5b22eb96bbdd80d4da1b559421224d1
wangjunyu200708 <2567232225@qq.com>
2026-05-10 20:29:01 +0800

    fix：mijia插件房间+设备控制修复

--- plugin/plugins/mijia/mijia_api/repositories/async_device_repository.py
+++ plugin/plugins/mijia/mijia_api/repositories/async_device_repository.py
@@ -312,7 +312,7 @@ class AsyncDeviceRepositoryImpl(IAsyncDeviceRepository):
             name=data.get("name", ""),
             model=data.get("model", ""),
             home_id=home_id,
-            room_id=data.get("roomid"),
+            room_id=data.get("roomid") or data.get("room_id"),
             status=status,
             parent_id=data.get("parent_id"),
             parent_model=data.get("parent_model"),
