cdcbb032747307f0ff85b277f99b8461c7a63ffa
LocalBuilder <local@builder>
2026-07-12 10:13:55 +0800

    refactor(plugin): remove legacy result stack

--- plugin/_types/__init__.py
+++ plugin/_types/__init__.py
@@ -1,15 +1,13 @@
 """
 插件系统统一类型定义
 
-提供所有公共类型、异常、Result 类型的统一导出。
+提供插件核心使用的公共类型和异常。
 这是 types/ 层的入口，所有类型定义都应该从这里导入。
 
 Usage:
     from plugin._types import (
         # 错误码
         ErrorCode, ERROR_NAMES, get_error_name, get_http_status,
-        # Result 类型
-        Ok, Err, Result, ResultError, safe, async_safe,
         # 异常
         PluginError, PluginNotFoundError, PluginTimeoutError,
         # 事件
@@ -32,20 +30,6 @@ from .errors import (
     ERROR_NAMES,
     get_error_name,
     get_http_status,
-)
-
-# Result 类型
-from .result import (
-    Ok,
-    Err,
-    Result,
-    ResultError,
-    safe,
-    async_safe,
-    try_call,
-    try_call_async,
-    from_optional,
-    collect_results,
 )
 
 # 异常
@@ -103,17 +87,6 @@ __all__ = [
     "ERROR_NAMES",
     "get_error_name",
     "get_http_status",
-    # Result 类型
-    "Ok",
-    "Err",
-    "Result",
-    "ResultError",
-    "safe",
-    "async_safe",
-    "try_call",
-    "try_call_async",
-    "from_optional",
-    "collect_results",
     # 异常
     "PluginError",
     "PluginNotFoundError",
