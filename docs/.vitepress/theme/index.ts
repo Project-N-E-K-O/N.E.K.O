4b1c93f954b8a79884f955a91ee8b8582396dcb1
Amadeus <838520558@qq.com>
2026-07-21 13:01:31 +0800

    docs: gate GA4 behind analytics consent

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
+import AnalyticsConsent from './AnalyticsConsent.vue'
 import LocaleSwitch from './LocaleSwitch.vue'
+import {
+  enableGoogleAnalytics,
+  trackAnalyticsPageView,
+} from './analytics-consent.mjs'
 import './custom.css'
 
 export default {
@@ -8,5 +13,16 @@ export default {
   Layout: () => h(DefaultTheme.Layout, null, {
     'nav-bar-content-after': () => h(LocaleSwitch, { variant: 'desktop' }),
     'nav-screen-content-after': () => h(LocaleSwitch, { variant: 'mobile' }),
+    'layout-bottom': () => h(AnalyticsConsent),
   }),
+  enhanceApp({ router }) {
+    if (typeof window === 'undefined') return
+
+    enableGoogleAnalytics()
+    const existingAfterRouteChange = router.onAfterRouteChange
+    router.onAfterRouteChange = async (to) => {
+      await existingAfterRouteChange?.(to)
+      trackAnalyticsPageView(to)
+    }
+  },
 }
